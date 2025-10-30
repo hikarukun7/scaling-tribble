9445f30d2398abdacbffea524aceeaedbdad2c68
hikarukun7 <121777569+hikarukun7@users.noreply.github.com>
2025-10-30 17:42:25 +0900

    Add files via upload

---
+++ Game/Renderer.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <d3d12.h>
+#include <dxgi1_4.h>
+
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+#include <vector>
+
+#include <wrl.h>
+using namespace Microsoft::WRL;
+
+
+class Renderer
+{
+public:
+	Renderer(class Game* game);
+	~Renderer();
+
+	bool initialize();
+
+	void begin();
+	void end();
+
+private:
+	static const UINT FrameNum = 2;
+	class Game* m_game;
+
+	ComPtr<IDXGIFactory4> m_dxgiFactory;
+	D3D_FEATURE_LEVEL m_featureLevel;
+	ComPtr<ID3D12Device>     m_device;
+	ComPtr<ID3D12CommandQueue> m_cmdQueue;
+	ComPtr<IDXGISwapChain3>    m_swapchain;
+	UINT                       m_bufferIndex;
+	ComPtr<ID3D12CommandAllocator> m_cmdAllocators[FrameNum];
+	ComPtr<ID3D12GraphicsCommandList> m_cmdList;
+	ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+	UINT                         m_rtvIncSize;
+	ComPtr<ID3D12Resource>       m_backBuffers[FrameNum];
+
+	bool createFactory();
+	bool createDevice(const wchar_t* adapterName);
+	bool createCommandQueue();
+	bool createSwapchain();
+	bool createCommandAllocators();
+	bool createCommandList();
+	bool createRenderTargetView();
+
+	void enableDebugLayer();
+
+};
+
+//70‚©‚ç
